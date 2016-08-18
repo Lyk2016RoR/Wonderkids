706918c42d1f866ac41ae94515db834e00184b4a
“Kıvanç <kivancdkmc@gmail.com>
2016-08-18 20:50:58 +0300

    form eklendi

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,42 @@
 class BooksController < ApplicationController
 
+		before_action :authenticate_user!, except: [:show, :index]
+     	before_action :set_book, only: [:show, :update, :edit, :destroy]
+     	before_action :authorize_user!, only: [:edit, :update, :destroy]
+
+def new
+		@book = Book.new
+		#load_categories
+		load_form_data
 
 def show
 	@books = Book.find(params[:id])
 end
+
+def create 
+		@book = current_user.books.new(book_params)
+		if @book.save
+			flash[:success] = 'İşlem başarıyla tamamlandı'
+			redirect_to book_path(@book)
+		else
+			#load_categories
+			load_form_data
+			render :new
+		end
+	end
+
+	def update
+		if @book.update(book_params)
+			redirect_to book_path(@book)
+		else
+			#load_categories
+			load_form_data
+			render :edit
+		end
+	end
+
+	def load_form_data
+		@categories = Category.all.collect {|c| [c.name, c.id]}
+	end
 
 end
